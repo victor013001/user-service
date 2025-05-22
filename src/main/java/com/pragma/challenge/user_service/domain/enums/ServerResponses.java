ffb136e1b045eb2055d0bcd57f4489c9823db736
victor013001 <victormanuelosoriog@gmail.com>
2025-05-22 13:09:34 -0500

    HU-7 chore: remove unused

--- src/main/java/com/pragma/challenge/user_service/domain/enums/ServerResponses.java
+++ src/main/java/com/pragma/challenge/user_service/domain/enums/ServerResponses.java
@@ -14,14 +14,12 @@ public enum ServerResponses {
       "E001",
       org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR,
       ConstantsMsg.SERVER_ERROR_MSG),
-  RESOURCE_NOT_FOUND(
-      "E002", org.springframework.http.HttpStatus.NOT_FOUND, ConstantsMsg.RESOURCE_NOT_FOUND_MSG),
   GATEWAY_ERROR(
-      "E003",
+      "E002",
       org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR,
       ConstantsMsg.GATEWAY_ERROR_MSG),
   GATEWAY_BAD_REQUEST(
-      "E004",
+      "E003",
       org.springframework.http.HttpStatus.BAD_REQUEST,
       ConstantsMsg.GATEWAY_BAD_REQUEST_MSG),
   SUCCESS_REGISTER_TO_BOOTCAMP(
